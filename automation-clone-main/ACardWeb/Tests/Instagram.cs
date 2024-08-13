3f21a9f6dec3c804b7e5bc7552eebcd274dab8a7
kevinrosario05 <66438794+kevinrosario05@users.noreply.github.com>
2024-08-13 14:34:35 -0400

    Subiendo 2d

--- automation-clone-main/ACardWeb/Tests/Instagram.cs
+++ automation-clone-main/ACardWeb/Tests/Instagram.cs
@@ -1,11 +1,15 @@
 ﻿using ACardWeb.Keywords;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
 
 namespace ACardWeb.Tests
 {
@@ -18,22 +22,28 @@ namespace ACardWeb.Tests
 
         public void IniciarSesion()
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+            options.AddAdditionalOption("useAutomationExtension", false);
 
-            Iniciar("CHROME");
+            driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl(Profiles.QA.URL);
+
             LoginMethods.IngresarUsuario(driver);
-           // MenuPrincipalMethods.IngresarUsuario(driver, Profiles.QA.Usuario, Profiles.QA.Clave);
-        
+
+            // MenuPrincipalMethods.IngresarUsuario(driver, Profiles.QA.Usuario, Profiles.QA.Clave);
+
             //Validaciones
         }
-     /*(   [TestMethod("Iniciar sesion con las credenciales incorrectas")]
-        public void IniciarSesion_CredencialesInorrectas()
-        {
-            Iniciar("CHROME");
-            LoginMethods.IngresarUsuario(driver, Profiles.QA.Usuario);
-            LoginMethods.IngresarClave(driver, "asasas");
-            LoginMethods.PresionarEntrar(driver);
-            //Validaciones
-        } */
+        /*(   [TestMethod("Iniciar sesion con las credenciales incorrectas")]
+           public void IniciarSesion_CredencialesInorrectas()
+           {
+               Iniciar("CHROME");
+               LoginMethods.IngresarUsuario(driver, Profiles.QA.Usuario);
+               LoginMethods.IngresarClave(driver, "asasas");
+               LoginMethods.PresionarEntrar(driver);
+               //Validaciones
+           } */
     }
 }
